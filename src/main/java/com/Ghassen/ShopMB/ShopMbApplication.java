ad2f3d2f5abc3a634cb9d3d36ae14b90c66403a5
GhassenMbarek511 <ghassenmbarek511@gmail.com>
2024-09-29 08:03:05 +0100

    initial setup

---
+++ src/main/java/com/Ghassen/ShopMB/ShopMbApplication.java
@@ -0,0 +1,13 @@
+package com.Ghassen.ShopMB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ghassen/ShopMB/ShopMbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ghassen.ShopMB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
